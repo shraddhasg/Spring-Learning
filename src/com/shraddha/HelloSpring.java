1c1483f18ff83ba2d82a1c9e107de919a2014b72
Shraddha Shinde <shraddha.s@FinIQ-SP.com>
2021-10-19 14:09:35 +0530

    Initial Commit

---
+++ src/com/shraddha/HelloSpring.java
@@ -0,0 +1,12 @@
+package com.shraddha;
+
+public class HelloSpring {
+    private String msg = "Good Morning";
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    public String getMsg() {
+        return msg;
+    }
+}
---
+++ src/com/shraddha/Main.java
@@ -0,0 +1,9 @@
+package com.shraddha;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
